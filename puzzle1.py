0d42f185bc7fe2269b9fa72b7680d7813983ae52
Kebabrolle42 <45644048+Kebabrolle42@users.noreply.github.com>
2018-12-06 04:15:52 +0100

    puzzle1.py

--- puzzle1.py
+++ puzzle1.py
@@ -1,16 +1,8 @@
 x=0
-
-f2 = open('puzzle1output.txt', 'r+')
-f3 = open('puzzle1output2.txt', 'r+')
-with open('puzzle1input.txt','r+') as f1, open('puzzle1output.txt', 'r+') as f2, open('puzzle1output2.txt', 'r+') as f3:
+with open('puzzle1input.txt','r+') as f1, open('puzzle1output.txt', 'r+') as f2:
         for line in f1:
                 x = x + int(line)
-                f2.write(str(x) +  " ")
-                for line in f2:
-                        for line in f2:
-                                if line == line:
-                                        f3.write(f2.readline())                    
+                f2.write(str(x) +  " ")                   
 
 f1.close
-f2.close
-f3.close
+
